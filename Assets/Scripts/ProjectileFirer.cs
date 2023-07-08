087ecd0fd9f4ed17aed4701e87288674947cb145
Akash Shah <akash.w.shah@gmail.com>
2023-07-08 00:12:23 -0400

    Mostly finish animating Kaiju and work on entity system

--- Assets/Scripts/ProjectileFirer.cs
+++ Assets/Scripts/ProjectileFirer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ProjectileFirer : MonoBehaviour
 {
-    public GameObject Fire(GameObject projectilePrefab, Vector2 direction)
+    public GameObject FireInDirection(GameObject projectilePrefab, Vector2 direction)
     {
         GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
         projectile.GetComponent<Projectile>().direction = direction.normalized;
@@ -13,6 +13,6 @@ public class ProjectileFirer : MonoBehaviour
 
     public GameObject FireAt(GameObject projectilePrefab, Vector2 direction)
     {
-        return Fire(projectilePrefab, (direction - new Vector2(transform.position.x, transform.position.y)).normalized);
+        return FireInDirection(projectilePrefab, (direction - new Vector2(transform.position.x, transform.position.y)).normalized);
     }
 }
